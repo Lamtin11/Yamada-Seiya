6d45b5b2fb04fee24465d4b9060188852808a62e
kai75 <kai75@LAPTOP-ADRJ1Q7I>
2025-06-04 21:58:04 +0900

    Sample

---
+++ HelloWorld_FamilynameFirstname/src/sample/SelfIntroduction.java
@@ -0,0 +1,20 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		//氏名
+		 String name="山田　聖也";
+		//年齢 
+		 int age=21;		 
+		//趣味 
+		 String hobby="ゲーム";		 
+				 
+		 System.out.println("私の名前は"+name+"です。");
+		 System.out.println("年齢は"+age+"歳です。");
+		 System.out.println("趣味は"+hobby+"です。");
+		 // TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
