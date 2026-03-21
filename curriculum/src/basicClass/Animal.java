75415aee0404be17f5385ad565e9f436e7791313
kai75 <kai75@LAPTOP-ADRJ1Q7I>
2026-03-22 00:49:36 +0900

    修正完了いたしました

--- curriculum/src/basicClass/Animal.java
+++ curriculum/src/basicClass/Animal.java
@@ -7,10 +7,11 @@ public class Animal {
 	
 	public static void main(String[] args) {
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
-		  Dog dog = new Dog("コン", 2);
+		  Dog dog = new Dog();
+		  Dog dog2=new Dog(3);
 	        System.out.println("名前は " + dog.name + " です。");
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
-	        System.out.println("数は " + dog.count + " です。");
+	        System.out.println("数は " + dog2.count + " です。");
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
 	        LocalDateTime now = LocalDateTime.now();
 	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd H:m:s");
