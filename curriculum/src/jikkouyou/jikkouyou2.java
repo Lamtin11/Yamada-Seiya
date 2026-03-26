91f76e9016fad31ff32d1d877e8dbb0862b39d30
kai75 <kai75@LAPTOP-ADRJ1Q7I>
2026-03-26 21:57:00 +0900

    課題22パッケージをコンパイルを完了いたしました。

---
+++ curriculum/src/jikkouyou/jikkouyou2.java
@@ -0,0 +1,12 @@
+package jikkouyou;
+
+import syoriyou.syoriyou2;
+
+public class jikkouyou2 {
+    public static void main(String[] args) {
+
+        syoriyou2 msg = new syoriyou2();
+        msg.printMessage();
+
+    }
+}
---
+++ curriculum/src/syoriyou/syoriyou2.java
@@ -0,0 +1,32 @@
+package syoriyou;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class syoriyou2 {
+
+    private String msg1;
+    private String msg2;
+    private String msg3;
+    private String now;
+
+    // コンストラクタ
+    public syoriyou2() {
+        this.msg1 = "こんにちは！ここは日本です！";
+        this.msg2 = "この寿司はうまい";
+        this.msg3 = "寿司は和食です";
+
+        // 現在日時を取得
+        LocalDateTime date = LocalDateTime.now();
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        this.now = date.format(fmt);
+    }
+
+    // 表示用メソッド
+    public void printMessage() {
+        System.out.println(this.msg1);
+        System.out.println(this.msg2);
+        System.out.println(this.msg3);
+        System.out.println("今の現在日時は" + this.now + "です");
+    }
+}
