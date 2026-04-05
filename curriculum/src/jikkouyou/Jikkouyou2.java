763a5f6367960dfb48c75dd4247b6d248788ee9f
kai75 <kai75@LAPTOP-ADRJ1Q7I>
2026-04-06 01:54:13 +0900

    課題2_22修正いたしました。

---
+++ curriculum/src/jikkouyou/Jikkouyou2.java
@@ -0,0 +1,13 @@
+package jikkouyou;
+
+import syoriyou.Syoriyou2;
+
+public class Jikkouyou2 {
+    public static void main(String[] args) {
+
+        Syoriyou2 msg = new Syoriyou2();
+        msg.printMessage();
+
+    }
+
+}
