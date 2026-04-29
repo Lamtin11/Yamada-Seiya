86ced3e435d2c07119ef86e76af3db2f7ee31ea1
kai75 <kai75@LAPTOP-ADRJ1Q7I>
2026-04-30 00:29:48 +0900

    課題30完了いたしました。

---
+++ curriculum/src/kadai2_30/Person.java
@@ -0,0 +1,26 @@
+package kadai2_30;
+
+public class Person {
+     String name;
+     int age;
+     double height;
+     double weight;
+     int count=0;
+    
+    Person(String name,int age,double height,double weight){
+    	this.name = name;
+        this.age = age;
+        this.height = height;
+    	this.weight = weight;
+        count++;
+}
+       public double bmi(){
+        return this.weight /(this.height * this.height);
+}
+       public void print() {
+    	   System.out.println("名前は" + this.name + "です");
+           System.out.println("年は" + this.age + "です");
+           System.out.println("BMIは" + this.bmi() + "です");
+           System.out.println("合計" + this.count + "人です");
+       }
+}
